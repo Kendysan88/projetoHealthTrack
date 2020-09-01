25cbd2d03ca8f19f83069d13654b96b0f4357004
Richard Kendy <email@email.com>
2020-09-01 17:51:22 -0300

    Criação da pasta backend e o projeto Java do HealthTrack.

---
+++ Backend/HealthTrack/src/br/com/healthtrack/Teste.java
@@ -0,0 +1,11 @@
+package br.com.healthtrack;
+
+public class Teste {
+	
+//	Nesta classe iremos testar as classes que foram criadas para o projeto.
+	public static void main(String[] args) {
+		
+
+	}
+
+}
