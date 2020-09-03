2c45b9f92b2f53d8a268c9dbde426ab95c53b897
AfonSir <afonsir17@gmail.com>
2020-09-02 22:17:06 -0300

    Refactoring: moving validations to setters

--- Backend/HealthTrack/src/br/com/healthtrack/meal/Food.java
+++ Backend/HealthTrack/src/br/com/healthtrack/meal/Food.java
@@ -49,38 +49,27 @@ public class Food {
 		return name;
 	}
 
-	public boolean isValid() {
-		if(!(this.amount > 0)) {
-			System.out.println("Food amount cannot be neither negative nor zero.");
-			return false;
-		}
-
-		if(!(this.calories > 0)) {
-			System.out.println("Food calories cannot be neither negative nor zero.");
-			return false;
-		}
-
-		if(this.name.equals(null) || this.name.trim().equals("")) {
-			System.out.println("Food name cannot be neither null nor blank.");
-			return false;
-		}
-
-		return true;
-	}
-
 	public void setAmount(double amount) {
-		this.amount = amount;
+		if(amount > 0) {
+			this.amount = amount;
+		}
 	}
 
 	public void setCalories(double calories) {
-		this.calories = calories;
+		if(calories > 0) {
+			this.calories = calories;
+		}
 	}
 	
 	public void setDescription(String description) {
-		this.description = description;
+		if(!description.trim().equals("")) {
+			this.description = description;
+		}
 	}
 
 	public void setName(String name) {
-		this.name = name;
+		if(!name.trim().equals("")) {
+			this.name = name;
+		}
 	}
 }
