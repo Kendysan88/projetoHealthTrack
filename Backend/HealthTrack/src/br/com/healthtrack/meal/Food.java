690eac457503307ebaa4149fce199dbd5c528303
AfonSir <afonsir17@gmail.com>
2020-09-02 21:13:40 -0300

    Adding Food class

---
+++ Backend/HealthTrack/src/br/com/healthtrack/meal/Food.java
@@ -0,0 +1,69 @@
+package br.com.healthtrack.meal;
+
+import br.com.healthtrack.utils.Constants;
+
+public class Food {
+
+	// keep in alphabetically sorted
+	// TODO: amount should be able to be dynamically declared.
+	private double amount;
+	private double calories;
+	private String description;
+	private String name;
+	
+	public Food() {
+	}
+	
+	public Food(double amount, double calories, String name) {
+		this.amount = amount;
+		this.calories = calories;
+		this.name = name;
+	}
+	
+	public Food(double amount, double calories, String name, String description) {
+		this.amount = amount;
+		this.calories = calories;
+		this.name = name;
+		this.description = description;
+	}
+	
+	private String formatCalories(double calories) {
+		return String.format("%.2f", calories) + " " + Constants.CALORIES_UNIT_SYMBOL;
+	}
+
+	public double getAmount() {
+		return amount;
+	}
+
+	public double getCalories() {
+		return calories;
+	}
+	
+	public String getCaloriesPretty() {
+		return formatCalories(calories);
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+
+	public void setCalories(double calories) {
+		this.calories = calories;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
