1b853d006966bc2bd2e55a19d76ce26ce661609b
AfonSir <afonsir17@gmail.com>
2020-09-08 20:59:15 -0300

    Adding Meal class (with javadoc)

--- Backend/HealthTrack/src/br/com/healthtrack/utils/Constants.java
+++ Backend/HealthTrack/src/br/com/healthtrack/utils/Constants.java
@@ -1,10 +1,19 @@
 package br.com.healthtrack.utils;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Constants {
 	public static final String CALORIES_UNIT_SYMBOL = "cal";
+	public static final String DATETIME_FORMAT = "dd-MM-yyyy HH:mm";
 	
 	public static String formatCalories(double calories) {
 		return String.format("%.2f", calories) + " " + CALORIES_UNIT_SYMBOL;
+	}
+	
+	public static String formatDateTime(LocalDateTime dateTime) {
+		DateTimeFormatter format = DateTimeFormatter.ofPattern(DATETIME_FORMAT);
+		return dateTime.format(format);
 	}
 }
 
