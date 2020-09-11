b2541cbf2b5b052841f20841b8ed0162b5e5aa98
Gabriel <gsouzapiscinato@gmail.com>
2020-09-11 19:59:44 -0300

    Addind User class

---
+++ Backend/HealthTrack/src/br/com/healthtrack/users/User.java
@@ -0,0 +1,16 @@
+package br.com.healthtrack.users;
+
+public class User extends Person {
+	
+	private String dataNascimento;
+
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	
+	
+	}
