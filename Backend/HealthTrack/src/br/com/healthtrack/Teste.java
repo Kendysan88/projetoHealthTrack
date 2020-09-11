4d68e9c755d70d8e02bb2b81c5e04722da3a2ec6
AfonSir <afonsir17@gmail.com>
2020-09-10 21:04:43 -0300

    Testing some classes

--- Backend/HealthTrack/src/br/com/healthtrack/Teste.java
+++ Backend/HealthTrack/src/br/com/healthtrack/Teste.java
@@ -7,10 +7,79 @@ import br.com.healthtrack.meal.FoodItem;
 import br.com.healthtrack.meal.Meal;
 
 public class Teste {
-	
-//	Nesta classe iremos testar as classes que foram criadas para o projeto.
-	public static void main(String[] args) {
 
+	public static void main(String[] args) {
+		Food beef  = new Food();
+
+		beef.setName("Beef");
+		beef.setAmount(100);
+		beef.setCalories(1000);
+		beef.save();
+
+		System.out.println(beef.getName() + ": " + beef.getCaloriesPretty());
+
+		// -------------------------
+
+		Food rice  = new Food();
+
+		rice.setName("Rice");
+		rice.setAmount(100);
+		rice.setCalories(400);
+		rice.save();
+
+		System.out.println(rice.getName() + ": " + rice.getCaloriesPretty());
+
+		// -------------------------
+
+		Food beans = new Food();
+
+		beans.setName("Beans");
+		beans.setAmount(100);
+		beans.setCalories(350);
+		beans.save();
+
+		System.out.println(beans.getName() + ": " + beans.getCaloriesPretty());
+
+		// -------------------------
+
+		Meal lunch = new Meal();
+
+		lunch.setType("LUNCH");
+		lunch.setDateTime(LocalDateTime.now());
+
+		// -------------------------
+
+		FoodItem riceItem = new FoodItem(rice);
+
+		riceItem.setAmount(200);
+
+		System.out.println(riceItem.getFoodName() + ": " + riceItem.getCaloriesPretty());
+
+		// -------------------------
+
+		FoodItem beefItem = new FoodItem(beef);
+
+		beefItem.setAmount(50);
+
+		System.out.println(beefItem.getFoodName() + ": " + beefItem.getCaloriesPretty());
+
+		// -------------------------
+
+		FoodItem beansItem = new FoodItem(beans);
+
+		beansItem.setAmount(180);
+
+		System.out.println(beansItem.getFoodName() + ": " + beansItem.getCaloriesPretty());
+
+		// -------------------------
+
+		lunch.getFoodItems().add(riceItem);
+		lunch.getFoodItems().add(beefItem);
+		lunch.getFoodItems().add(beansItem);
+
+		System.out.println("Total calories: " + lunch.getTotalCaloriesPretty());
+
+		lunch.save();
 	}
 
 }
