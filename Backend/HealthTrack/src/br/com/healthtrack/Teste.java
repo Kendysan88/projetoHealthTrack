671d237fc238509a415497a3da1eed1a0a77767d
Richard Kendy <email@email.com>
2020-09-11 18:48:17 -0300

    Adicionado ao projeto as classes PhysicalActivity, PhysicalActivityBySeries e PhysicalActivityByDuration.

--- Backend/HealthTrack/src/br/com/healthtrack/Teste.java
+++ Backend/HealthTrack/src/br/com/healthtrack/Teste.java
@@ -1,9 +1,41 @@
 package br.com.healthtrack;
 
+import br.com.healthtrack.physicalactivity.PhysicalActivity;
+import br.com.healthtrack.physicalactivity.PhysicalActivityByDuration;
+import br.com.healthtrack.physicalactivity.PhysicalActivityBySeries;
+
 public class Teste {
 	
 //	Nesta classe iremos testar as classes que foram criadas para o projeto.
 	public static void main(String[] args) {
+		System.out.println("MOSTRANDO ATIVIDADES FÍSICAS");
+		PhysicalActivity atividadeFisica = new PhysicalActivity(2000, 14, 16, "Corrida");
+		System.out.println(atividadeFisica.calculateCalories(2000));
+		System.out.println(atividadeFisica.getBeginHour());
+		System.out.println(atividadeFisica.getEndHour());
+		System.out.println(atividadeFisica.getType());
+		
+//		System.out.println("MOSTRANDO ATIVIDADES FÍSICAS POR SÉRIE");
+//		PhysicalActivityBySeries atividadeSeries = new PhysicalActivityBySeries(2000, 16, 14, "Musculação", 3, 15, 20);
+//		System.out.println(atividadeSeries.calculateCalories(2000));
+//		System.out.println(atividadeSeries.getBeginHour());
+//		System.out.println(atividadeSeries.getEndHour());
+//		System.out.println(atividadeSeries.getType());
+//		System.out.println(atividadeSeries.getSeries());
+//		System.out.println(atividadeSeries.getRepetions());
+//		System.out.println(atividadeSeries.getWeight());
+//		
+//		System.out.println("MOSTRANDO ATIVIDADES FÍSICAS POR DURAÇÃO");
+//		PhysicalActivityByDuration atividadePorDuracao = new PhysicalActivityByDuration(2000, 16, 14, "Ciclismo", 5);
+//		System.out.println(atividadePorDuracao.calculateCalories(2000));
+//		System.out.println(atividadePorDuracao.getBeginHour());
+//		System.out.println(atividadePorDuracao.getEndHour());
+//		System.out.println(atividadePorDuracao.getType());
+//		System.out.println(atividadePorDuracao.CalculateDuration());
+//		System.out.println(atividadePorDuracao.getDistance());
+
+		
+		
 		
 
 	}
