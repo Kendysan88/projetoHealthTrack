7a493183b42874ce8ec3fdb2e2c41abaf7dfd27b
AfonSir <afonsir17@gmail.com>
2020-09-23 22:03:15 -0300

    Adding TODO

--- Backend/HealthTrack/src/br/com/healthtrack/Teste.java
+++ Backend/HealthTrack/src/br/com/healthtrack/Teste.java
@@ -2,9 +2,10 @@ package br.com.healthtrack;
 
 import java.time.LocalDateTime;
 
-import br.com.healthtrack.meal.Food;
-import br.com.healthtrack.meal.FoodItem;
-import br.com.healthtrack.meal.Meal;
+import br.com.healthtrack.model.bodyinformation.BloodPreassure;
+import br.com.healthtrack.model.meal.Food;
+import br.com.healthtrack.model.meal.FoodItem;
+import br.com.healthtrack.model.meal.Meal;
 import br.com.healthtrack.physicalactivity.PhysicalActivity;
 import br.com.healthtrack.physicalactivity.PhysicalActivityByDuration;
 import br.com.healthtrack.physicalactivity.PhysicalActivityBySeries;
@@ -17,77 +18,75 @@ import br.com.healthtrack.physicalactivity.PhysicalActivityBySeries;
 public class Teste {
 //	Nesta classe iremos testar as classes que foram criadas para o projeto.
 	public static void main(String[] args) {
-		Food beef  = new Food();
-
-		beef.setName("Beef");
-		beef.setAmount(100);
-		beef.setCalories(1000);
-		beef.save();
-
-		System.out.println(beef.getName() + ": " + beef.getCaloriesPretty());
-
-		// -------------------------
-
-		Food rice  = new Food();
-
-		rice.setName("Rice");
-		rice.setAmount(100);
-		rice.setCalories(400);
-		rice.save();
-
-		System.out.println(rice.getName() + ": " + rice.getCaloriesPretty());
-
-		// -------------------------
-
-		Food beans = new Food();
-
-		beans.setName("Beans");
-		beans.setAmount(100);
-		beans.setCalories(350);
-		beans.save();
-
-		System.out.println(beans.getName() + ": " + beans.getCaloriesPretty());
-
-		// -------------------------
-
-		Meal lunch = new Meal();
-
-		lunch.setType("LUNCH");
-		lunch.setDateTime(LocalDateTime.now());
-
-		// -------------------------
-
-		FoodItem riceItem = new FoodItem(rice);
-
-		riceItem.setAmount(200);
-
-		System.out.println(riceItem.getFoodName() + ": " + riceItem.getCaloriesPretty());
-
-		// -------------------------
-
-		FoodItem beefItem = new FoodItem(beef);
-
-		beefItem.setAmount(50);
-
-		System.out.println(beefItem.getFoodName() + ": " + beefItem.getCaloriesPretty());
-
-		// -------------------------
-
-		FoodItem beansItem = new FoodItem(beans);
-
-		beansItem.setAmount(180);
-
-		System.out.println(beansItem.getFoodName() + ": " + beansItem.getCaloriesPretty());
-
-		// -------------------------
-
-		lunch.getFoodItems().add(riceItem);
-		lunch.getFoodItems().add(beefItem);
-		lunch.getFoodItems().add(beansItem);
-
-		System.out.println("Total calories: " + lunch.getTotalCaloriesPretty());
-
-		lunch.save();
+// TODO: fix it!
+//		Food beef  = new Food();
+//		beef.setName("Beef");
+//		beef.setAmount(100);
+//		beef.setCalories(1000);
+//		beef.save();
+//
+//		System.out.println(beef.getName() + ": " + beef.getCaloriesPretty());
+//
+//		// -------------------------
+//
+//		Food rice  = new Food();
+//
+//		rice.setName("Rice");
+//		rice.setAmount(100);
+//		rice.setCalories(400);
+//		rice.save();
+//
+//		System.out.println(rice.getName() + ": " + rice.getCaloriesPretty());
+//
+//		// -------------------------
+//
+//		Food beans = new Food();
+//
+//		beans.setName("Beans");
+//		beans.setAmount(100);
+//		beans.setCalories(350);
+//		beans.save();
+//
+//		System.out.println(beans.getName() + ": " + beans.getCaloriesPretty());
+//
+//		// -------------------------
+//
+////		Meal lunch = new Meal();
+//
+////		lunch.setType("LUNCH");
+////		lunch.setDateTime(LocalDateTime.now());
+//
+//		// -------------------------
+//
+//		FoodItem riceItem = new FoodItem(rice);
+//
+//		riceItem.setAmount(200);
+//
+//		System.out.println(riceItem.getFoodName() + ": " + riceItem.getCaloriesPretty());
+//
+//		// -------------------------
+//
+//		FoodItem beefItem = new FoodItem(beef);
+//
+//		beefItem.setAmount(50);
+//
+//		System.out.println(beefItem.getFoodName() + ": " + beefItem.getCaloriesPretty());
+//
+//		// -------------------------
+//
+//		FoodItem beansItem = new FoodItem(beans);
+//
+//		beansItem.setAmount(180);
+//
+//		System.out.println(beansItem.getFoodName() + ": " + beansItem.getCaloriesPretty());
+//
+//		// -------------------------
+//
+////		lunch.getFoodItems().add(riceItem);
+////		lunch.getFoodItems().add(beefItem);
+////		lunch.getFoodItems().add(beansItem);
+//
+////		System.out.println("Total calories: " + lunch.getTotalCaloriesPretty());
 		
 		System.out.println("MOSTRANDO ATIVIDADES FÍSICAS");
 		PhysicalActivity atividadeFisica = new PhysicalActivity(2000, 14, 16, "Corrida");
