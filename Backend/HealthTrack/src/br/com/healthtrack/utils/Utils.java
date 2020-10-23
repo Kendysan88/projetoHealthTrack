5f7675c51cf2d5b3e261879674c3dec3673aaa2a
AfonSir <afonsir17@gmail.com>
2020-10-22 21:41:15 -0300

    Adding deprecation TODO

--- Backend/HealthTrack/src/br/com/healthtrack/utils/Utils.java
+++ Backend/HealthTrack/src/br/com/healthtrack/utils/Utils.java
@@ -5,13 +5,19 @@ import java.time.format.DateTimeFormatter;
 
 public class Utils {
 	public static final int PASSWORD_SIZE = 6;
+	// TODO: Deprecated
 	public static final String CALORIES_UNIT_SYMBOL = "cal";
 	public static final String DATETIME_FORMAT = "dd-MM-yyyy HH:mm:ss";
+	// TODO: Deprecated
 	public static final String GRAMS_UNIT_SYMBOL = "g";
+	// TODO: Deprecated
 	public static final String KILOGRAMS_UNIT_SYMBOL = "kg";
+	// TODO: Deprecated
 	public static final String MERCURY_MILLIMETERS_UNIT_SYMBOL = "mmHg";
+	// TODO: Deprecated
 	public static final String METERS_UNIT_SYMBOL = "m";
 	
+	// TODO: Deprecated
 	public static String formatCalories(double calories) {
 		return String.format("%.2f", calories) + " " + CALORIES_UNIT_SYMBOL;
 	}
@@ -21,14 +27,17 @@ public class Utils {
 		return dateTime.format(format);
 	}
 
+	// TODO: Deprecated
 	public static String formatGrams(double value) {
 		return String.format("%.2f", value) + " " + GRAMS_UNIT_SYMBOL;
 	}
 
+	// TODO: Deprecated
 	public static String formatKilograms(double value) {
 		return String.format("%.2f", value) + " " + KILOGRAMS_UNIT_SYMBOL;
 	}
 
+	// TODO: Deprecated
 	public static String formatMeters(double value) {
 		return String.format("%.2f", value) + " " + METERS_UNIT_SYMBOL;
 	}
