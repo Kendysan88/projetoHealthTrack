ef5808b84b1dd1037390b1a4b9b24e960d2f6595
AfonSir <afonsir17@gmail.com>
2020-10-25 02:23:01 -0300

    Refactoring: improving food and its dao

--- Backend/HealthTrack/src/br/com/healthtrack/TesteDAO.java
+++ Backend/HealthTrack/src/br/com/healthtrack/TesteDAO.java
@@ -8,8 +8,6 @@ import br.com.healthtrack.model.meal.Meal;
 import br.com.healthtrack.model.meal.MealDAO;
 import br.com.healthtrack.model.meal.MealType;
 import br.com.healthtrack.model.meal.MealTypeDAO;
-import br.com.healthtrack.model.measurementunit.MeasurementUnit;
-import br.com.healthtrack.model.measurementunit.MeasurementUnitDAO;
 import br.com.healthtrack.model.physicalactivity.PhysicalActivity;
 import br.com.healthtrack.model.physicalactivity.PhysicalActivityDAO;
 
@@ -21,29 +19,6 @@ import br.com.healthtrack.model.physicalactivity.PhysicalActivityDAO;
 public class TesteDAO {
 
 	public static void main(String[] args) {
-		System.out.println("Unidade de Medida DAO");
-
-		MeasurementUnitDAO unitDAO = new MeasurementUnitDAO();
-
-		MeasurementUnit unit1 = new MeasurementUnit("kg", "quilogramas");
-		unitDAO.create(unit1);
-
-		MeasurementUnit unit2 = new MeasurementUnit("m", "metros", "Usada nas medições de altura");
-		unitDAO.create(unit2);
-
-		MeasurementUnit unit3 = new MeasurementUnit("cal", "calorias", "Utilizada nas medições de atividade físicas");
-		unitDAO.create(unit3);
-
-		MeasurementUnit unit4 = new MeasurementUnit("g", "gramas");
-		unitDAO.create(unit4);
-
-		MeasurementUnit unit5 = new MeasurementUnit("ml", "mililitros");
-		unitDAO.create(unit5);
-
-		for(MeasurementUnit unitItem : unitDAO.getAll()) {
-			System.out.println(unitItem.getInfoPretty());
-		}
-
 		System.out.println("\n\nTipos de Refeição DAO");
 
 		MealTypeDAO mealTypeDAO = new MealTypeDAO();
@@ -68,37 +43,37 @@ public class TesteDAO {
 
 		FoodDAO foodDAO = new FoodDAO();
 
-		Food food1 = new Food(30, 198, "Bacon frito", unit4, "2 cubos.");
+		Food food1 = new Food(30, 198, "Bacon frito", "g", "2 cubos.");
 		foodDAO.create(food1);
 
-		Food food2 = new Food(100, 144, "Coxa de frango", unit4, "1 unidade.");
+		Food food2 = new Food(100, 144, "Coxa de frango", "g", "1 unidade.");
 		foodDAO.create(food2);
 
-		Food food3 = new Food(100, 287, "Picanha", unit4, "1 fatia.");
+		Food food3 = new Food(100, 287, "Picanha", "g", "1 fatia.");
 		foodDAO.create(food3);
 
-		Food food4 = new Food(50, 371, "Bolo de cenoura com cobertura de chocolate", unit4, "1 fatia.");
+		Food food4 = new Food(50, 371, "Bolo de cenoura com cobertura de chocolate", "g", "1 fatia.");
 		foodDAO.create(food4);
 
-		Food food5 = new Food(200, 1074, "Chocolate meio-amargo", unit4, "1 unidade.");
+		Food food5 = new Food(200, 1074, "Chocolate meio-amargo", "g", "1 unidade.");
 		foodDAO.create(food5);
 
-		Food food6 = new Food(100, 68, "Gelatina", unit4, "1 porção.");
+		Food food6 = new Food(100, 68, "Gelatina", "g", "1 porção.");
 		foodDAO.create(food6);
 
-		Food food7 = new Food(350, 230, "Manga", unit4, "1 unidade.");
+		Food food7 = new Food(350, 230, "Manga", "g", "1 unidade.");
 		foodDAO.create(food7);
 
-		Food food8 = new Food(20, 4, "Alface", unit4, "2 folhas.");
+		Food food8 = new Food(20, 4, "Alface", "g", "2 folhas.");
 		foodDAO.create(food8);
 
-		Food food9 = new Food(100, 139, "Lasanha", unit4, "1 porção.");
+		Food food9 = new Food(100, 139, "Lasanha", "g", "1 porção.");
 		foodDAO.create(food9);
 
-		Food food10 = new Food(140, 304, "Pizza de mussarela", unit4, "1 fatia.");
+		Food food10 = new Food(140, 304, "Pizza de mussarela", "g", "1 fatia.");
 		foodDAO.create(food10);
 
-		Food food11 = new Food(250, 161, "Suco de uva", unit5, "1 copo.");
+		Food food11 = new Food(250, 161, "Suco de uva", "ml", "1 copo.");
 		foodDAO.create(food11);
 
 		for(Food foodItem : foodDAO.getAll()) {
--- Backend/HealthTrack/src/br/com/healthtrack/model/meal/Food.java
+++ Backend/HealthTrack/src/br/com/healthtrack/model/meal/Food.java
@@ -1,29 +1,37 @@
 package br.com.healthtrack.model.meal;
 
-import br.com.healthtrack.model.measurementunit.MeasurementUnit;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import br.com.healthtrack.utils.Utils;
 
 /**
- * Classe que abstrai uma porção de um item alimentício, bem como seu valor calórico.
+ * Classe que abstrai uma porção de um item alimentício, bem como seu
+ * valor calórico.
  * @author Afonso de Sousa Costa
- * @version 3.1
+ * @version 4.0
  */
 public class Food implements Comparable<Food> {
 
 	private double amount;
 	private double calories;
 	private String description;
+	private int id;
 	private String name;
-	private MeasurementUnit unit;
+	private String unit;
+	private static List<String> validUnits = new ArrayList<String>(
+			Arrays.asList( "g", "ml" ));
 	
 	/**
-	 * Método construtor padrão (amount, calories, unidade de medida e name são obrigatórios).
+	 * Método construtor padrão (amount, calories, unidade de medida
+	 * e name são obrigatórios).
 	 * @param amount   Quantidade em gramas do item alimentício.
 	 * @param calories Quatidade de calorias do item alimentício.
 	 * @param name     Nome do item alimentício.
 	 * @param unit     Unidade de medida do item alimentício.
 	 */
-	public Food(double amount, double calories, String name, MeasurementUnit unit) {
+	public Food(double amount, double calories, String name, String unit) {
 		setAmount(amount);
 		setCalories(calories);
 		setName(name);
@@ -31,14 +39,54 @@ public class Food implements Comparable<Food> {
 	}
 
 	/**
-	 * Método construtor padrão sobrecarregado (amount, calories, unidade de medida, name e description são obrigatórios).
+	 * Método construtor padrão sobrecarregado (amount, calories,
+	 * unidade de medida, name e description são obrigatórios).
 	 * @param amount      Quantidade em gramas do item alimentício.
 	 * @param calories    Quatidade de calorias do item alimentício.
 	 * @param name        Nome do item alimentício.
 	 * @param unit        Unidade de medida do item alimentício.
 	 * @param description Descrição/observação do item alimentício.
 	 */
-	public Food(double amount, double calories, String name, MeasurementUnit unit, String description) {
+	public Food(double amount, double calories, String name,
+			String unit, String description) {
+		setAmount(amount);
+		setCalories(calories);
+		setName(name);
+		setUnit(unit);
+		setDescription(description);
+	}
+
+	/**
+	 * Método construtor padrão (amount, calories, unidade de medida
+	 * e name são obrigatórios).
+	 * @param amount   Quantidade em gramas do item alimentício.
+	 * @param calories Quatidade de calorias do item alimentício.
+	 * @param id       Identificador do item alimentício.
+	 * @param name     Nome do item alimentício.
+	 * @param unit     Unidade de medida do item alimentício.
+	 */
+	public Food(int id, double amount, double calories,
+			String name, String unit) {
+		setId(id);
+		setAmount(amount);
+		setCalories(calories);
+		setName(name);
+		setUnit(unit);
+	}
+
+	/**
+	 * Método construtor padrão sobrecarregado (amount, calories,
+	 * unidade de medida, name e description são obrigatórios).
+	 * @param amount      Quantidade em gramas do item alimentício.
+	 * @param calories    Quatidade de calorias do item alimentício.
+	 * @param id          Identificador do item alimentício.
+	 * @param name        Nome do item alimentício.
+	 * @param unit        Unidade de medida do item alimentício.
+	 * @param description Descrição/observação do item alimentício.
+	 */
+	public Food(int id, double amount, double calories,
+			String name, String unit, String description) {
+		setId(id);
 		setAmount(amount);
 		setCalories(calories);
 		setName(name);
@@ -60,16 +108,18 @@ public class Food implements Comparable<Food> {
 	}
 
 	/**
-	 * Método para se obter a quantidade do item alimentício com sufixo de unidade.
-	 * @return Quantidade do item alimentício em gramas (g).
+	 * Método para se obter a quantidade do item alimentício
+	 * com sufixo de unidade.
+	 * @return Quantidade do item alimentício em gramas (g),
+	 * ou, em mililitros (ml).
 	 */
 	public String getAmountPretty() {
-		return unit.formatValueToUnit(amount);
+		return amount + " " + unit;
 	}
 
-
 	/**
-	 * Método para se obter a quantidade de calorias do item alimentício.
+	 * Método para se obter a quantidade de calorias do item
+	 * alimentício.
 	 * @return Quantidade de calorias (cal) do item alimentício.
 	 */
 	public double getCalories() {
@@ -77,7 +127,8 @@ public class Food implements Comparable<Food> {
 	}
 
 	/**
-	 * Método para se obter a quantidade de calorias do item alimentício com sufixo de unidade.
+	 * Método para se obter a quantidade de calorias do item
+	 * alimentício com sufixo de unidade.
 	 * @return Quantidade de calorias (cal) do item alimentício.
 	 */
 	public String getCaloriesPretty() {
@@ -93,6 +144,14 @@ public class Food implements Comparable<Food> {
 	}
 
 	/**
+	 * Método para se obter o identificador do item alimentício.
+	 * @return Identificador do item alimentício.
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
 	 * Método para se obter informações sobre o objeto instanciado.
 	 * @return Texto contendo os valores dos atributos desse objeto.
 	 */
@@ -102,10 +161,12 @@ public class Food implements Comparable<Food> {
 		if (getDescription() != null){
 			info = getName() +
 					" (" + getDescription() + ") "
-					+ getAmountPretty() + " - " + getCaloriesPretty();
+					+ getAmountPretty() + " - "
+					+ getCaloriesPretty();
 		} else {
 			info = getName() +
-					" " + getAmountPretty() + " - " + getCaloriesPretty();
+					" " + getAmountPretty() + " - "
+					+ getCaloriesPretty();
 		}
 
 		return info;
@@ -124,11 +185,12 @@ public class Food implements Comparable<Food> {
 	 * @return Unidade de medida do item alimentício.
 	 */
 	public String getUnitPrefix() {
-		return unit.getUnitPrefix();
+		return unit;
 	}
 
 	/**
-	 * Método para se alterar a quantidade do item alimentício (apenas para valores maiores do que zero).
+	 * Método para se alterar a quantidade do item alimentício
+	 * (apenas para valores maiores do que zero).
 	 * @param amount Quantidade em gramas do item alimentício.
 	 */
 	public void setAmount(double amount) {
@@ -136,7 +198,8 @@ public class Food implements Comparable<Food> {
 	}
 
 	/**
-	 * Método para se alterar a quantidade de calorias do item alimentício (apenas para valores maiores do que zero).
+	 * Método para se alterar a quantidade de calorias do item
+	 * alimentício (apenas para valores maiores do que zero).
 	 * @param calories Quantidade de calorias do item alimentício.
 	 */
 	public void setCalories(double calories) {
@@ -144,7 +207,8 @@ public class Food implements Comparable<Food> {
 	}
 	
 	/**
-	 * Método para se alterar a descrição do item alimentício (apenas para valores diferentes de vazio).
+	 * Método para se alterar a descrição do item alimentício
+	 * (apenas para valores diferentes de vazio).
 	 * @param description Descrição do item alimentício.
 	 */
 	public void setDescription(String description) {
@@ -156,7 +220,18 @@ public class Food implements Comparable<Food> {
 	}
 
 	/**
-	 * Método para se alterar o nome do item alimentício (apenas para valores diferentes de vazio).
+	 * Método para se alterar o identificador do item alimentício.
+	 * @param id Identificador do item alimentício.
+	 */
+	public void setId(int id) {
+		if(id > 0) {
+			this.id = id;
+		}
+	}
+
+	/**
+	 * Método para se alterar o nome do item alimentício
+	 * (apenas para valores diferentes de vazio).
 	 * @param name Nome do item alimentício.
 	 */
 	public void setName(String name) {
@@ -168,10 +243,17 @@ public class Food implements Comparable<Food> {
 	}
 
 	/**
-	 * Método para se alterar a unidade de medida do item alimentício (apenas para valores diferentes de vazio).
+	 * Método para se alterar a unidade de medida do item
+	 * alimentício (apenas para valores diferentes de vazio,
+	 * e que estejam dentro da lista de unidades válidas).
 	 * @param unit Unidade de medida do item alimentício.
 	 */
-	public void setUnit(MeasurementUnit unit) {
-		this.unit = unit;
+	public void setUnit(String unit) {
+		unit = unit.trim();
+
+		if(!unit.equals("") &&
+				validUnits.contains(unit.toLowerCase())) {
+			this.unit = unit;
+		}
 	}
 }
