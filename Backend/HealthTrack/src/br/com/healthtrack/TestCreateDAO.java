e69a7c236cc22acd3e7eef86135dc2e569e82d7c
Richard Kendy <kendysan@gmail.com>
2020-10-26 19:27:15 -0300

    Update Master
    Removing the Teste, TestaDAO and TesteDeConexao classes and renaming the TesteCadastroDAO to TestCreateDAO and TesteListaDAO to TestGetAllDAO

--- Backend/HealthTrack/src/br/com/healthtrack/TestCreateDAO.java
+++ Backend/HealthTrack/src/br/com/healthtrack/TestCreateDAO.java
@@ -2,14 +2,27 @@ package br.com.healthtrack;
 
 import java.time.LocalDateTime;
 
+import br.com.healthtrack.model.meal.Food;
+import br.com.healthtrack.model.meal.FoodDAO;
+import br.com.healthtrack.model.meal.FoodItem;
+import br.com.healthtrack.model.meal.FoodItemDAO;
+import br.com.healthtrack.model.meal.Meal;
+import br.com.healthtrack.model.meal.MealDAO;
+import br.com.healthtrack.model.meal.MealType;
+import br.com.healthtrack.model.meal.MealTypeDAO;
 import br.com.healthtrack.model.physicalactivity.PhysicalActivityBySeries;
 import br.com.healthtrack.model.physicalactivity.dao.PhysicalActivityBySeriesDAOImpl;
 
-public class TesteCadastroDAO {
+/**
+ * Classe que serve para inserir os registros nas tabelas que foram criadas.
+ * @author Todos os membros do grupo
+ * @version 2.0
+ */
+public class TestCreateDAO {
 
 	public static void main(String[] args) {
 		
-		//Instancia DAO
+		//Instancia PhysicalActivityBySeriesDAO
 		PhysicalActivityBySeriesDAOImpl dao = new PhysicalActivityBySeriesDAOImpl();
 
 		
@@ -91,6 +104,78 @@ public class TesteCadastroDAO {
 		//Cadastra no banco de dados
 		dao.create(physicalActivity6);		
 	    System.out.println("Cadastrado!");
+	    
+		//Instancia MealTypeDAO
+	    MealTypeDAO mealTypeDAO = new MealTypeDAO();
+	    
+	    //Cadastra no banco de dados
+		MealType mealType1 = new MealType("café da manhã");
+		mealTypeDAO.create(mealType1);
+
+		MealType mealType2 = new MealType("almoço");
+		mealTypeDAO.create(mealType2);
+
+		MealType mealType3 = new MealType("jantar");
+		mealTypeDAO.create(mealType3);
+
+		MealType mealType4 = new MealType("lanche");
+		mealTypeDAO.create(mealType4);
+		
+		//Instancia FoodDAo
+		FoodDAO foodDAO = new FoodDAO();
+
+	    //Cadastra no banco de dados
+		Food food1 = new Food(30, 198, "Bacon frito", "g", "2 cubos.");
+		foodDAO.create(food1);
+
+		Food food2 = new Food(100, 144, "Coxa de frango", "g", "1 unidade.");
+		foodDAO.create(food2);
+
+		Food food3 = new Food(100, 287, "Picanha", "g", "1 fatia.");
+		foodDAO.create(food3);
+
+		Food food4 = new Food(50, 371, "Bolo de cenoura com cobertura de chocolate", "g", "1 fatia.");
+		foodDAO.create(food4);
+
+		Food food5 = new Food(200, 1074, "Chocolate meio-amargo", "g", "1 unidade.");
+		foodDAO.create(food5);
+
+		Food food6 = new Food(100, 68, "Gelatina", "g", "1 porção.");
+		foodDAO.create(food6);
+
+		Food food7 = new Food(350, 230, "Manga", "g", "1 unidade.");
+		foodDAO.create(food7);
+
+		Food food8 = new Food(20, 4, "Alface", "g", "2 folhas.");
+		foodDAO.create(food8);
+
+		Food food9 = new Food(100, 139, "Lasanha", "g", "1 porção.");
+		foodDAO.create(food9);
+
+		Food food10 = new Food(140, 304, "Pizza de mussarela", "g", "1 fatia.");
+		foodDAO.create(food10);
+
+		Food food11 = new Food(250, 161, "Suco de uva", "ml", "1 copo.");
+		foodDAO.create(food11);
+		
+		//Instancia a Meal
+		Meal meal1 = new Meal(LocalDateTime.now(), mealType2);
+		
+		//Instancia MealDAO
+		MealDAO mealDAO = new MealDAO();
+
+	    //Adiciona alimentos na refeição
+		FoodItem foodItem1 = new FoodItem(food1, 200);
+		meal1.addFoodItem(foodItem1);
+
+		FoodItem foodItem2 = new FoodItem(food2, 150);
+		meal1.addFoodItem(foodItem2);
+
+		FoodItem foodItem3 = new FoodItem(food3, 100);
+		meal1.addFoodItem(foodItem3);
+
+		//Cadastra no banco de dados
+		mealDAO.create(meal1, 1);
 	    
 	}
 
