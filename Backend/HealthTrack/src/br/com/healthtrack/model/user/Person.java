c27f3fbf5eb869e3af7babb76830dbb693e924b1
AfonSir <afonsir17@gmail.com>
2020-11-03 20:40:44 -0300

    Refactoring: improving person class

--- Backend/HealthTrack/src/br/com/healthtrack/model/user/Person.java
+++ Backend/HealthTrack/src/br/com/healthtrack/model/user/Person.java
@@ -4,51 +4,72 @@ import br.com.healthtrack.utils.Utils;
 
 /**
  * Classe que abstrai uma pessoa.
- * @author Gabriel Souza Piscinato
- * @version 1.0
+ * @author Afonso de Sousa Costa
+ * @version 2.0
  */
 
-public class Person {
+public abstract class Person {
 
     private String email;
     private String password;
 
+    /**
+	 * Método construtor.
+	 */
     public Person() {
     }
 
-    /**
-     * Método construtor padrão.
-     * @param email Email da pessoa.
-     * @param password Senha da pessoa.
-     */
-
+	/**
+	 * Método construtor (email e password são obrigatórios).
+     * @param email 	Email da pessoa.
+     * @param password 	Senha da pessoa.
+	 */
     public Person(String email, String password) {
-        this.email = email;
-        this.password = password;
+        setEmail(email);
+        setPassword(password);
     }
 
+	/**
+	 * Método para se obter o email da pessoa.
+	 * @return Email da pessoa.
+	 */
     public String getEmail() {
         return this.email;
     }
 
-    public String getPassword() {
-        return this.password;
+    /**
+	 * Método para se alterar a senha da pessoa.
+	 * (apenas para valores diferentes de vazio).
+	 * @param password Senha da pessoa.
+	 */
+	public void setPassword(String password) {
+		password = password.trim();
+
+		if(!password.equals("") && password.length() >=
+				Utils.PASSWORD_SIZE) {
+			this.password = password;
+		}
     }
 
-    public void setPassword(String password, String passwordConfirmation) {
-        if(!password.trim().equals("") &&
-        	password.trim().length() >= Utils.PASSWORD_SIZE &&
-        	password.trim().equals(passwordConfirmation.trim())) {
-            this.password = password;
-        }
-    }
-
+	/**
+	 * Método para se alterar o email da pessoa.
+	 * (apenas para valores diferentes de vazio).
+	 * @param email Email da pessoa.
+	 */
     public void setEmail(String email) {
-        if(!email.trim().equals("") && email.trim().contains("@")) {
-            this.email = email;
-        }
+		email = email.trim();
+
+		if(!email.equals("")) {
+			this.email = email;
+		}
     }
 
-    
-
+    /**
+	 * Método para se validar a senha da pessoa.
+	 * @param password Senha a ser comparada à da pessoa.
+	 * @return Verdadeiro se ambas as senha coincidem.
+	 */
+    public boolean validatePassword(String password) {
+        return this.password.equalsIgnoreCase(password.trim());
+    }
 }
